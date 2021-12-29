38eb944d6775d6c8157d1acc8180177792182ef1
KhoiNgLe <nguy0572@algonquinlive.com>
2021-12-29 17:02:55 -0500

    add styles

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        >npm i sass
           Learn React
         </a>
       </header>
