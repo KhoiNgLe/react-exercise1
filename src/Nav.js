a1d14190c22c6c6c48bf432c0661eca90de831a4
KhoiNgLe <nguy0572@algonquinlive.com>
2021-12-29 16:42:07 -0500

    First commit

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css'
+const Nav = () => {
+
+  return (
+    <div>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav;
